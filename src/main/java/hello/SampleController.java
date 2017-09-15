7f07acbd0d4ea786297c779e9c63cc4d73238cd4
Spock <snah@riotgames.com>
2017-09-15 21:25:21 +0900

    Introduce environment message

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+    @Value("${environment.message}")
+    private String message;
 
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello World! This is " + message ;
     }
 
     public static void main(String[] args) throws Exception {
