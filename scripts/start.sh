4e95d7c3d25d31addf8fcfb8a607d21dd8bcd0a9
Spock <snah@riotgames.com>
2017-09-17 19:14:14 +0900

    Modify start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,4 @@ then
 fi
 sudo cp -f $WORKSPACE/target/*.jar /home/vagrant/$1
 cd /home/vagrant/$1
-sudo nohup java -Xmx128M -jar -Dapplication.name=$1 /home/vagrant/$1/*.jar > /dev/null 2> /dev/null < /dev/null &
+sudo nohup java -Xmx128M -jar -Dapplication.name=$1 /home/vagrant/$1/*.jar &
