1e43a55b64728cf102953da4ec467da654b2680c
Spock <snah@riotgames.com>
2017-09-17 18:53:38 +0900

    Modify start/stop script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if ! [ -d /home/vagrant/$1 ];
+then
+  sudo mkdir /home/vagrant/$1
+fi
 sudo cp -f $WORKSPACE/target/*.jar /home/vagrant/$1
 cd /home/vagrant/$1
 sudo nohup java -Xmx128M -jar -Dapplication.name=$1 /home/vagrant/$1/*.jar > /dev/null 2> /dev/null < /dev/null &
